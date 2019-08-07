8907ca43717063b1bd44e47a0964cf429c90df38
tiago <tiagolpadua@gmail.com>
2019-08-06 22:11:59 -0300

    exemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,72 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng2-smtb-exemple';
+
+  settings = {
+    columns: {
+      id: {
+        title: 'ID'
+      },
+      name: {
+        title: 'Full Name'
+      },
+      username: {
+        title: 'User Name',
+        type: 'html',
+        editor: {
+          type: 'list',
+          config: {
+            list: [
+              {
+                value: 'Antonette',
+                title: 'Antonette'
+              },
+              {
+                 value: 'Bret',
+                  title: 'Bret'
+                },
+              {
+                value: 'Samantha',
+                title: 'Samantha',
+              }],
+          },
+        },
+      },
+      email: {
+        title: 'Email'
+      }
+    }
+  };
+
+  data = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 11,
+      name: 'Nicholas DuBuque',
+      username: 'Nicholas.Stanton',
+      email: 'Rey.Padberg@rosamond.biz'
+    }
+  ];
+
+  constructor(private http: HttpClient) {
+    setTimeout(() => {
+      this.http.get('/assets/data.json')
+      .subscribe((data: string[]) => {
+        console.log(data);
+        this.settings.columns.username.editor.config.list = data.map(nome => ({value: nome, title: nome}));
+        this.settings = Object.assign({}, this.settings );
+      });
+    }, 1000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { Ng2CompleterModule } from 'ng2-completer';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2SmartTableModule,
+    Ng2CompleterModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
